2cc1e419a56f96b053167e6d78ac552f4a337d86
mohamed <mobabeke@student.42wolfsburg.de>
2023-11-06 10:15:42 +0100

    Bin to Decimal

---
+++ binToDecimal.js
@@ -0,0 +1,9 @@
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+// The solution
+function binToDec(bin) {
+  // Use parseInt to convert the binary string to a decimal number with base 2.
+  const decimalNumber = parseInt(bin, 2);
+
+  return decimalNumber;
+}
