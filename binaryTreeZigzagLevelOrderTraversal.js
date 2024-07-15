93f012fd78d62ef5564a382d12c1a95f77ab73d7
mohamed <mobabeke@student.42wolfsburg.de>
2024-07-15 06:22:38 +0100

    Binary Tree Zigzag Level Order Traversal

---
+++ binaryTreeZigzagLevelOrderTraversal.js
@@ -0,0 +1,63 @@
+// Given the root of a binary tree, return the zigzag level order traversal of its nodes' values. (i.e., from left to right, then right to left for the next level and alternate between).
+
+// Example 1:
+
+// Input: root = [3,9,20,null,null,15,7]
+// Output: [[3],[20,9],[15,7]]
+// Example 2:
+
+// Input: root = [1]
+// Output: [[1]]
+// Example 3:
+
+// Input: root = []
+// Output: []
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [0, 2000].
+// -100 <= Node.val <= 100
+
+// The solution
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var zigzagLevelOrder = function (root) {
+  if (!root) return [];
+
+  let result = [];
+  let queue = [root];
+  let leftToRight = true;
+
+  while (queue.length > 0) {
+    let levelSize = queue.length;
+    let currentLevel = [];
+
+    for (let i = 0; i < levelSize; i++) {
+      let node = queue.shift();
+
+      if (leftToRight) {
+        currentLevel.push(node.val);
+      } else {
+        currentLevel.unshift(node.val);
+      }
+
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+
+    result.push(currentLevel);
+    leftToRight = !leftToRight;
+  }
+
+  return result;
+};
