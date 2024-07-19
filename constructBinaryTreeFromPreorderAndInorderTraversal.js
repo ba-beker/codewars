908a23056178f37915d875ec3b244717d79167b7
mohamed <mobabeke@student.42wolfsburg.de>
2024-07-19 05:00:11 +0100

    Construct Binary Tree from Preorder and Inorder Traversal

---
+++ constructBinaryTreeFromPreorderAndInorderTraversal.js
@@ -0,0 +1,51 @@
+// Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
+
+// Example 1:
+
+// Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+// Output: [3,9,20,null,null,15,7]
+// Example 2:
+
+// Input: preorder = [-1], inorder = [-1]
+// Output: [-1]
+
+// Constraints:
+
+// 1 <= preorder.length <= 3000
+// inorder.length == preorder.length
+// -3000 <= preorder[i], inorder[i] <= 3000
+// preorder and inorder consist of unique values.
+// Each value of inorder also appears in preorder.
+// preorder is guaranteed to be the preorder traversal of the tree.
+// inorder is guaranteed to be the inorder traversal of the tree
+
+// The solution
+function buildTree(preorder, inorder) {
+  if (!preorder.length || !inorder.length) return null;
+
+  // The first element of preorder is the root
+  const rootVal = preorder[0];
+  const root = new TreeNode(rootVal);
+
+  // Find the index of the root in inorder
+  const rootIndex = inorder.indexOf(rootVal);
+
+  // Recursively build the left and right subtrees
+  root.left = buildTree(
+    preorder.slice(1, rootIndex + 1),
+    inorder.slice(0, rootIndex)
+  );
+  root.right = buildTree(
+    preorder.slice(rootIndex + 1),
+    inorder.slice(rootIndex + 1)
+  );
+
+  return root;
+}
+
+// Definition for the binary tree node
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
+}
