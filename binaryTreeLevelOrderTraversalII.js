2a3469fa9ef1a93c7cb471884f96abf9d574b947
mohamed <mobabeke@student.42wolfsburg.de>
2024-07-22 06:40:15 +0100

    Binary Tree Level Order Traversal II

---
+++ binaryTreeLevelOrderTraversalII.js
@@ -0,0 +1,64 @@
+// Given the root of a binary tree, return the bottom-up level order traversal of its nodes' values. (i.e., from left to right, level by level from leaf to root).
+
+// Example 1:
+
+// Input: root = [3,9,20,null,null,15,7]
+// Output: [[15,7],[9,20],[3]]
+// Example 2:
+
+// Input: root = [1]
+// Output: [[1]]
+// Example 3:
+
+// Input: root = []
+// Output: []
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [0, 2000].
+// -1000 <= Node.val <= 1000
+
+// The solution
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+class TreeNode {
+  constructor(val, left = null, right = null) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+const levelOrderBottom = (root) => {
+  if (!root) return [];
+
+  const queue = [root];
+  const result = [];
+
+  while (queue.length > 0) {
+    const levelSize = queue.length;
+    const currentLevel = [];
+
+    for (let i = 0; i < levelSize; i++) {
+      const node = queue.shift();
+      currentLevel.push(node.val);
+
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+
+    result.unshift(currentLevel); // Insert the current level at the beginning
+  }
+
+  return result;
+};
