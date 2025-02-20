daa125d7395d20d055818f5eca581abb91166c43
mohamed <mobabeke@student.42wolfsburg.de>
2025-02-20 15:17:37 +0100

    Sort List

---
+++ sortList.js
@@ -0,0 +1,73 @@
+// Given the head of a linked list, return the list after sorting it in ascending order.
+
+// Example 1:
+
+// Input: head = [4,2,1,3]
+// Output: [1,2,3,4]
+// Example 2:
+
+// Input: head = [-1,5,3,4,0]
+// Output: [-1,0,3,4,5]
+// Example 3:
+
+// Input: head = []
+// Output: []
+
+// Constraints:
+
+// The number of nodes in the list is in the range [0, 5 * 104].
+// -105 <= Node.val <= 105
+
+// The solution
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var sortList = function (head) {
+  if (!head || !head.next) return head;
+
+  // Split the list into two halves
+  let slow = head,
+    fast = head,
+    prev = null;
+  while (fast && fast.next) {
+    prev = slow;
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  prev.next = null; // Cut the list into two halves
+
+  // Recursively sort both halves
+  let left = sortList(head);
+  let right = sortList(slow);
+
+  // Merge the sorted halves
+  return merge(left, right);
+};
+
+var merge = function (l1, l2) {
+  let dummy = new ListNode();
+  let current = dummy;
+
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
+      current.next = l1;
+      l1 = l1.next;
+    } else {
+      current.next = l2;
+      l2 = l2.next;
+    }
+    current = current.next;
+  }
+
+  current.next = l1 || l2;
+
+  return dummy.next;
+};
